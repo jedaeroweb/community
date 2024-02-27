4072524e99d4359873fab92e797c0ab6368da3c1
sleepinglion <admin@jedaeroweb.co.kr>
2024-02-28 01:07:56 +0900

    수정함:        app/views/questions/show.html.erb 	수정함:        db/migrate/20220820084307_create_answers.rb

--- db/migrate/20220820084307_create_answers.rb
+++ db/migrate/20220820084307_create_answers.rb
@@ -3,6 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.references :question, null: false
       t.references :user, null: false
+      t.boolean :accepted, null: false, default: false
       t.boolean :enable, null: false, default: true
       t.timestamps null: false
     end
