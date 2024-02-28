30aedb765c18c0d244016855bf801a200fa51a8e
sleepinglion <admin@jedaeroweb.co.kr>
2024-02-29 01:06:35 +0900

    수정함:        app/views/blogs/show.html.erb 	수정함:        app/views/questions/show.html.erb 	수정함:        app/views/talks/show.html.erb 	수정함:        db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -339,7 +339,8 @@ BlogCategory.create!(id: 2, title: '제작')
 BlogCategory.create!(id: 3, title: '테스트')
 BlogCategory.create!(id: 4, title: '배포')
 
-Blog.create!(id: 1, user_id:1, program_language_id: 2, blog_category_id:1, title: '루비설치',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png"))}, content: '루비 온 레일즈 프로그램을 설치하려면 당연히 루비 부터 설치해야 됩니다.
+Blog.create!(id: 1, user_id:1, program_language_id: 2, blog_category_id:1, title: '루비 설치 방법',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png"))}, content: '
+루비 온 레일즈 프로그램을 설치하려면 당연히 루비 부터 설치해야 됩니다.
           이름부터가 루비 온 레일즈로 루비로 만들어진 프레임워크입니다.
           루비를 설치하기 위해서는 여러 선택지가 존재하며 각 OS마다 약간의 차이가 있습니다.
 
@@ -351,17 +352,90 @@ Blog.create!(id: 1, user_id:1, program_language_id: 2, blog_category_id:1, title
   renv,rvm 을 이용한 설치방법을 설명해드리겠습니다.
 
 ')
-Blog.create!(id: 2, user_id:1, program_language_id: 2, blog_category_id:1, title: 'gem,bundler', content: 'gem은 ruby의 패키지 관리자로
-  <code>gem install 설치할프로그램 이름</code>
-이렇게 입력하고 실행하면 프로그램이 설치 됩니다.
+Blog.create!(id: 2, user_id:1, program_language_id: 2, blog_category_id:1, title: 'gem,bundler', content: '
+rubygem(Gem)이란 루비에서 지원하는 패키지 시스템으로 명령어만 입력하면 인터넷에서 자동으로 프로그램을 받아서 설치를 해줍니다.
 
-bundler는 gem의 의존성관리를 위한 프로그램
-  <code>gem install bundler</code>
+명령어는 gem으로
+
+인스톨시에는 gem install 패키지명
+업데이트시에는 gem update 패키지명
+삭제시에는 gem uninstall 패키지명
+
+
+레일즈에서는 이를 통해서 의존성 프로그램들을 설치하는데 예를 들면
+gem install rails를 실행하면 레일즈가 설치됩니다.
+그후에 "rails new app이름"  명령어를 실행하면 레일즈 app이 해당 이름으로 설치됩니다.
+
+
+일일히 귀찮게 gem설치를 할수 없기에 한번에 해결해주는 프로그램이 있는데 bundler라는 프로그램입니다.
+bundler는 gem의 의존성관리를 위한 프로그램으로
+<code>gem install bundler</code>
 이렇게 입력하고 실행하여 설치합니다.
-이렇게 설치하면 bundle이란 명령어가 생성되며
 
- <code>bundle install</code>을 통해 한번에 여러 gem 프로그램들을 설치할수 있습니다.',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
-Blog.create!(id: 3, user_id:1, program_language_id: 2, blog_category_id:1, title: '레일즈 설치,시작', content: '레일즈도 gem으로 되어 있어 gem install rails하면 설치가 끝납니다.' ,blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
+이렇게 설치하면 bundle이란 명령어가 생성되며
+<code>bundle install</code>을 통해 한번에 여러 gem 프로그램들을 설치할수 있습니다.
+최상위 폴더를 보면 Gemfile파일이 있는데 여기에 필요프로그램을 기술하면 알아서 설치를 해주는 것입니다.
+
+bundle install, bundle update 명령어를  통해 일괄적으로 처리가 가능합니다.
+이를 통해 Rails App에 필요한 프로그램들을  설치하여 배치시 의존성 문제를 쉽게 처리할수 있습니다.',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
+Blog.create!(id: 3, user_id:1, program_language_id: 2, blog_category_id:1, title: '루비온레일즈 소개,설치 방법', content: '
+웹개발을 하다보면 손이 가는게 한두가지가 아닙니다.
+처음에는 재미로 배우면서 한다지만  계속적인 많고 반복적인 작업이 개발자를 더욱 괴롭게했습니다.
+이렇게 반복적인 웹개발 패턴으로 괴로워하는 개발자를 구원하는 광명이 비추었으니
+그 이름은 "Ruby On Rails"  줄여서 RoR, Rails(레일즈) 라고 합니다.
+
+웹개발의 이런 어려움을 통합으로 해결해주는 "프레임워크"입니다.
+프레임워크라는 생소한 단어가 나왔습니다만
+건축으로 치면  뼈대를 제공한다고 보면 되겠습니다.
+우리는 이 뼈대위에서 작업을하면 그 뼈대를 만들기 위해 들이는 노력(고통?)을 들이지 않아도 되어
+보다 편하고 빠르게 결과물을 얻을수 있는것이지요
+물론 레일즈 전에도 이런 프레임워크가 없던것은 아닙니다.
+하지만 레일즈 처럼 깔끔하고 통합적으로 처리해주던 프레임워크는 그전까지 없었습니다.
+
+레일즈가 나온후에 반응 역시 폭발적으로
+그동안 그리 관심받지 못했던 Ruby언어의 관심과 사용이 급증했으며(레일즈는 Ruby란 언어로 제작되었고 Ruby를 사용하여 계속적으로 작업하게됩니다)
+이에 영감을 받은 다른 언어버젼 프레임워크들도 등장하게 됩니다.
+PHP버젼 Rails라 할수있는 CakePHP가 대표적이며 Laravel도 비슷한점이 너무 많음을 볼수 있습니다.
+그밖에 그 뒤로 나온 프레임워크들은 거의 다 영향을 받았지요
+어떤가요? Ruby On Rails를 더욱더 배워보고 싶지 않은가요?
+
+그럼 지금 바로 시작해봐요
+
+공식 홈페이지는 여기고요
+https://rubyonrails.org/
+
+
+윈도우나 맥에서에서 레일즈를 설치하기
+여기를 이용하면 한번에 설치가 되어서 정말 편리 합니다.
+
+https://www.railsinstaller.org
+
+
+리눅스에서 레일즈 설치하기
+리눅스에서 레일즈  설치하기는 정말  쉽습니다.
+단지 루비와 젬만 설치되어 있으면  됩니다.
+
+gem install rails
+
+
+이렇게 해서 레일즈가  설치되었다면   IDE설치를 하면  되겠습니다.
+물론 레일즈는 IDE 없이도 기본  텍스트  편집기로도 아무 문제 없이  사용할수 있지만  그래도  있으면 훨씬  편리하겠지요
+
+https://code.visualstudio.com/
+대세 에디터인 VSCode를 다운받고에서 ruby,rails 플러그인 다운받아 설피하면 됩니다.
+
+ IDE가  설치되었다면 New Rail Project를 실행하면 새로운 레일즈  프로젝트가  생성되고요
+ 그냥 명렁어 줄에서 하려면 새로운  프로젝트를 만들 디렉토리로 이동후
+
+ rails new  프로젝트명
+ 을 치시면  됩니다.
+
+ 이렇게  프로젝트 생성후에는 IDE에서 server 관련 탭으로 가면 해당  프로젝트 서버를  실행시킬수 있으며
+ 명렁어로는 ./bin/rails server를  실행시키면 개발 서버가 실행되므로 이를 이용하여 개발을 해 나가면  됩니다.
+
+ 기본적으로 서버는 3000번 포트를 이용하게 되어있으므로(물론 바꾸면 바꾼대로)  서버 실행후 웹 브라우져로
+ http://localhost:3000  로 접속되면 레일즈 설치가 완료  된것입니다.
+' ,blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
 Blog.create!(id: 4, user_id:1, program_language_id: 2, blog_category_id:1, title: 'rake migrate',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) }, content: 'rake는 레일즈의 여러 필요작업들을 처리해주는 명령어로 어느 책에서 봤던것 처럼 든든한 조수를 데리고 작업하는 것과 같은 느낌입니다.
 rake(조수) 이것(옵션) 처리해줘  하는 식으로 명령어를 주면 레일즈를 제작, 배치 하면서 처리해야 하는 일들을 편리하게 처리해 줍니다.
 
@@ -447,8 +521,76 @@ Blog.create!(id: 7, user_id:1, program_language_id: 2, blog_category_id:1, title
 보통 passenger패키지를 통해서 이루어 지며
 해당 내용 여기서 설명하기 보다 이곳을 참고해주세요',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
 Blog.create!(id: 8, user_id:1, program_language_id: 2, blog_category_id:4, title: 'git', content: 'git은 프로그램 버전관리 도구로 알려져 있으며 프로그램개발에서 빠질수 없는 너무나 중요한 것입니다.',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
-Blog.create!(id: 9, user_id:1, program_language_id: 2, blog_category_id:4, title: '카피스트라노(capistrano)를 이용한 배포', content: 'capistrano는 원래 완성된 레일즈를 배치(deploy)하기 위한 도구이며 너무나 편리하게(1줄의 명령어)로 배치를 자동으로 진행할 수 있습니다.
-이제는 레일즈외에 어떤한 프로그램이라도 배치 할수 있습니다.',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
+Blog.create!(id: 9, user_id:1, program_language_id: 2, blog_category_id:4, title: '카피스트라노(capistrano)를 이용한 자동 배포',
+             content: "
+웹프로그래머를 괴롭히는 또 하나의 과정은 배포(deploy) 입니다.
+배포 역시 처음엔 처음이라 어려워서 힘들지만 익숙해진 후에도 반복적일이 프로그래머의 정신을 황폐하게 만드는 작업이였지요.
+배포의 과정은 보통 이런 식이였지요
+로칼에서 작업한것을 FTP나 버전관리시스템에 올리고 서버에 접속하여 서버에서 필요한 수 많은 여러 작업을 해주고 재시작을 해야겠지요
+레일즈의 경우는 보통 배치시 서버에서 bundle update, rake db:migrate, rake assets:precompile등의 작업을 해주어야 되지요
+이런 반복적인일은 자동화의  필요성이 절실해집니다.
+그래서 만들어진 프로그램 카피스트라노(capistrano)를 소개합니다.
+이제 카피스트라노를 이용하면 저런 과정 필요없이 단 한줄의 명령어
+cap productioin deploy
+
+만 치면 배치는 완료됩니다.
+
+카피스트라노 역시 Gem으로 설치하면 됩니다.
+최상위 Gemfile 아래 내용을 입력하고
+
+group :development, :test do
+  gem 'capistrano', '~> 3.0'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails', '~> 1.0.0'
+end
+
+bundle update를 쳐주면 카피스트라노가 설치됩니다.
+잘 설치되었다면 cap 명령어가 생겼을겁니다.
+cap install 을 쳐주면 내 레일즈 어플에 설정 파일이 생성됩니다.
+
+우선 최상위에 Capfile은 사용할 기능 모듈 로드에 관한 내용인데
+
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
+
+보통은 이 세가지만 있으면 될 듯합니다.
+주석을 제거해주세요~
+다음은 config/deploy.rb의 내용을 봐야하는데
+여기가 거의 모든 설정을 처리하는 파일입니다.
+참 많은 내용이 있습니다만 역시 보통 사용시 필요한 부분만 보겠습니다.
+
+set :application, '어플리케이션 이름'
+set :repo_url, '레포지터리 경로'
+set :branch, '브랜치명'
+set :deploy_to, '서버에 배치될 경로'
+# set :scm, :git
+# set :format, :pretty
+# set :log_level, :debug
+set :pty, true
+set :linked_files, %w{config/database.yml .env}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/assets}
+
+
+처음은 어플리케이션이름, 레포지터리 경로, 브랜치명, 서버에 배치될 경로를 적고
+밑에 linked_files, linked_dirs에는 공유될 파일, 디렉토리를 적습니다.
+보통 버젼이 바뀌어도 안 바뀔 파일, 디렉토리, 임시파일등을 적어주면 됩니다.
+
+config/deploy 디렉토리에는
+production.rb  staging.rb 두 파일이 있는데
+그 안에는 접속에 필요한 사용자명, 서버명등을 넣으면 됩니다.
+
+이렇게 준비를 마치면  이제 카피스트라노를 이용한 배치 설정을 마친것입니다.
+이제 단순한 반복작업은 카피스트라노에게 맡기십시오
+
+cap production deploy
+단 한줄만 치면 배치가 끝납니다.
+
+혹시나 한번에 안되거나 더 많은 내용을 알고 싶다면
+카피스트라노 홈페이지(https://www.capistranorb.com)를 방문해보세요
+
+이제 배포시 반복작업을 없애주는 카피스트라노로 한번에 자동배포 하세요
+ ",blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "rails_welcome.png")) })
 
 QuestionCategory.create!(id: 1, title: '서버 설치,설정')
 QuestionCategory.create!(id: 2, title: '프레임워크, 패키지 설치, 설정')
@@ -667,6 +809,106 @@ M1에어로 아이폰 빌드 하는대도 기존의 인텔맥의 1/5 시간 정�
 ', created_at: 2.day.ago)
 
 
+Talk.create!(id: 12, user_id: 1, title: 'IIS제한 있었네요', content: '
+저는 원래 서버나 데스크탑이나 리눅스를 사용해왔고 따라서 Apache,Nginx등을 사용했지만
+어쩔수 없이 윈도우를 쓰게될경우 웹서버로는 윈도우 기본 제공인 IIS를 사용해왔습니다.
+제어판/프로그램 추가에서 따로 IIS선택해서 설치해야되긴 하지만 &quot;웹플랫폼 관리자&quot;와 같이 쓸경우에는 편하고 좋다고 생각했습니다. 그래서 IIS도 추천하기도 하고 했는데....
+
+그런데!....
+
+서버제품(Windows Server)이 아닌 데스크탑제품(windows7,8,10)의 IIS에는 접속제한이 있던것이였습니다.
+찾아보니 최대 접속이 10개로 되어있다고 합니다.
+어쩐지 테스트할때도 &quot;서비스 사용할 수 없음&quot;이 한번씩 뜨더라니....&nbsp;
+
+그럼 데스크탑일때는 숫자라도 맞게 표시되면 좋았을꺼를...&nbsp;&nbsp; 저거는 왜 저리 해놨는지 모르겠습니다.
+생각해보니 &quot;서버용&quot; 제품이 그래서 따로 있었구나 했습니다.&nbsp; 이렇게 생각하면 제가 바보 같지만...
+리눅스에서 무제한으로 웹서버를 쓰던 저에게는 아직도 이해가 안되는 정책이긴 합니다.
+이제 윈도우도 무료정책도 많이 쓰는것 같던데 웹서버에는 아직도 무료로 갈 생각이 별로 없나봅니다.
+아직까지는 데스크탑용 윈도우에서는 IIS는 개발,테스트 용도로만 사용해야겠습니다.
+한때 &quot;IIS도 좋구나&quot; 생각했던 저의 짝사랑이 깨지는 순간이였습니다
+MS야 이제 데탑,서버 구분없이 통합으로 만들어 내고 IIS 접속제한 없이 만들면 좋지 않겠니?
+
+서버제품(Windows Server)이 아니면 IIS는 개발,테스트 용도로만 쓰세요~', created_at: 7.day.ago)
+
+
+
+
+Talk.create!(id: 13, user_id: 1, title: '자동테스트를 써야 프로그래머입니다.', content: '
+셀레늄 안 쓰고 일일히 클릭하여 테스트 하는 사람은 웹프로그래머 아니잖아요?
+테스트 클릭 노가다꾼인가요?
+
+폼 몇개만 되어도 변경때 마다 일일히 테스트 하려면 시간 다 잡어 먹잖아요?
+테스트가 다 잘 된다는 보장도 없고요
+사용자가 변경후 애러 찾아줘서 고치면 사용자로부터 신뢰가 다 없어진 다음이지요
+프로그램은 애러 고친다고 해도 사용자로부터의 신뢰는 못 고치지요
+
+저도 부끄럽게도 여지껏  위처럼 지냈고 사용자가 애러 다 찾아줬습니다.
+이번에 셀레늄을 이용한 자동화 테스트를 만들어서 변경사항이 일어나도 프로그램 전체를 일일히 클릭하지 않고 테스트 할수 있게 하였습니다.
+저는 이제 웹프로그래머 입니다.
+
+아직 셀레늄 안 쓰시는 분들은 지금 바로 시작하세요
+각 언어별로 플러그인 다 나와있으니,  각자 자신에게 가장 잘 맞는것으로 해서 시작하세요', created_at: 7.day.ago)
+
+
+Talk.create!(id: 14, user_id: 1, title: '가성비 가상서버 마련하세요', content: '
+최고의 가성비 가상서버 업체 Vultr를 쓴지도 어느새 2년이 넘어가네요
+이렇게 좋은 서비스를 잘 이용하고 있긴 했지만
+도쿄리전을 쓰는거라 약간의 아쉬움이 있었는데..
+뭐 그렇다고 속도에는 전혀 문제는 느껴지지는 않았습니다
+
+그러던 중 보니 서울리전이 생겼었네요, 작년 5월에 생겼는데 그걸 모르고 있었네요....
+이번에 저도 서울리전 생생해서 서버이전 했네요
+이제는 너무나 익숙한 서버이전 작업,  하루도 안 걸려서 완전히 마쳤습니다.
+이제는 일본서버 왔다갔다 하는 시간도 필요없네요~ ㅋㅋㅎ
+속도는 빨라진것 모르겠지만 빨라졌겠지요??
+
+<a href="https://www.vultr.com/?ref=8793281-6G">나도 가상서버 만들러 vultr.com으로 가기</a>
+여러분들도 지금 바로 가입해서 가상서버를 마련하세요', created_at: 7.day.ago)
+
+
+
+
+Talk.create!(id: 15, user_id: 1, title: '자동테스트를 써야 프로그래머입니다.',
+content: 'IS+PHP를 위해서 웹플랫폼 관리자를 설치하고
+IIS인터넷 정보 서비스 관리자에서 경로설정후에 웹에서 localhost에 접속했지만 나타나는것은 애러~
+
+무슨일인지 살펴보니 권한문제로 해당폴더에 IIS_USRS,IUSR에 권한이 있어야 한다고 한다.
+그래서 권한을 주려고 하는데 그런 사용자가 없다고 한다.
+
+이게 무슨일인가? 해서 계속 이것저것 해보았지만 그런 사용자가 없어서 되지가 않았다.
+뭐 다른거 해야되나 해서 이것저것 다 해보아도 되는거는 없이 시간만 가고....
+
+거의 포기 하려고 하는 순간!, 보이는 스펠링 차이
+IIS_USERS,IUSER로 입력했던것이다.
+있지도 않은 E가 왜 있어 보였는지.....  미칠 노릇이였다.
+이렇게 어이없이 오타로 인해서 몇시간 삽질한 바보같은 나였다.
+
+여러분도 오타, 오인식 조심하세요~~
+', created_at: 7.day.ago)
+
+Talk.create!(id: 16, user_id: 1, title: '자동테스트를 써야 프로그래머입니다.',
+content: '사용자가 비디오 파일을 업로드하고 그내용 볼수 있게 하는 게시판을 만들고 있었습니다.
+
+  이미 이전에도 했던 내용이라 별 어러움 없이 처리 될줄 알았는데 안되는겁니다!!
+<video width="넓이" height="높이" controls>
+<source src="업로드한 mp4경로" type="video/mp4">
+<source src="업로드한 ogg경로" type="video/ogg">
+  Your browser does not support the video tag.
+</video>
+
+이런 내용이었는데 안될 이유가 없는데 안 되는겁니다. ㅜ.ㅜ
+이리저리 왜 안되는가를 보니.........
+
+웹서버 mineType에 등록이 안되어 있어서 그랬습니다.
+이전 서버에는 등록이 되어있었는데 이 서버에는 등록이 안되어있던겁니다.
+다시 생각하면 당연히 그런것을 생각못하고 프로그램이 잘못되었는가 하면서 또 삽질을 몇시간 했습니다.
+
+mineType등록은 설정 파일에(보통 따로 mineTyp설정 파일이 있음)
+AddType video/mpeg  .mp4
+이런식으로 추가하고 적용시키면 됩니다.
+
+결론 = Video태그 쓰려면 서버에 mineType등록 되어 있는 파일 형식이여야 된다.
+', created_at: 7.day.ago)
 
 Market.create!(id: 1, user_id: 4, title: '워드프레스사이트 제작', content: '
 워드프레스를 이용한 사이트 제작을 하려고 합니다.
@@ -683,6 +925,265 @@ Rails로 만들어진 사이트 1년동안 유지보수 맡아주실분 구합�
 ', created_at: 2.day.ago)
 
 
+
+
+
 Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '워드프레스를 위한 PHP 설치, 설정', content: '워드프레스 설치는 이렇게',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp")) })
 Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '워드프레스 설치,설정', content: '워드프레스' ,blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp")) })
 Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '워드프레스 시작하기', content: '워드프레스 시작하기',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp")) })
+Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '워드프레스 리플리케이션 플러그인',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp"))},
+content: "
+홈페이지가 Wordpress로 운영되고 있고
+2대 이상의 DB서버가 있어서 부하분산을 하고싶다면 바로 이 플러그인을 사용하면 됩니다.
+<a href='https://wordpress.org/plugins/hyperdb' target='_blank'>HyperDB</a>
+
+
+
+물론 2대 이상의 DB서버는 MySQL Replication으로 복제가 되어 서로 언제나 같아야 겠지요
+MySQL Replication에 대해서는 많은 자료가 있기에 따로 설명하지 않겠습니다.
+
+
+HyperDB플러그인을 받게되면 2개의 파일이 있는데
+
+db.php와 db-config.php입니다.
+이름으로 금방 알수 있겠지만 설정파일과 실제적으로 작동하는 파일입니다.
+
+홈페이지에서는 설명하기로는
+db.php 파일을 wp-content폴더 밑에 가져다 놓는것으로 설치는 끝이라고 하네요(실제로 그렇습니다)
+설정파일은 어디에 있던지 지정만 해주면 상관없지만
+
+저는 wp-config.php와 같은 위치에 놓고
+wp-config.php의
+
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
+
+밑부분에
+
+define('DB_CONFIG_FILE', ABSPATH.'db-config.php');
+로 추가해서 db-config.php파일을 찾을수 있게 했습니다.
+
+db-config.php은 커넥션들을 설정해주는것인데
+
+저의 설정은
+
+$wpdb->add_database(array(
+    'host'     => DB_HOST,     // If port is other than 3306, use host:port.
+    'user'     => DB_USER,
+    'password' => DB_PASSWORD,
+    'name'     => DB_NAME,
+    'write'    => 1,
+    'read'     => 1,
+    'dataset'  => 'global',
+    'timeout'  => 0.2,
+));
+
+$wpdb->add_database(array(
+    'host'     => DB_SLAVE_1,     // If port is other than 3306, use host:port.
+    'user'     => 'wordpress_ro',
+    'password' => 'wordpress_ro',
+    'name'     => DB_NAME,
+    'write'    => 0,
+    'read'     => 1,
+    'dataset'  => 'global',
+    'timeout'  => 0.2,
+));
+
+이런식으로 되어 있습니다.
+슬레이브에는 당연히 write는 0으로 해야겠지요?
+저의 경우는 보다시피 계정도 Read Only권한을 가진 계정으로 접속하게 설정했습니다.
+마스터의 경우는 읽기부하를 받게 할지 안할지는 선택적으로 처리하면 될듯합니다.
+저의 경우는 2대밖에 없지만 많은 DB서버가 있다면  Replication과  HyperDb플러그인으로
+Wordpress홈페이지의 DB부하 분산은 걱정은 끝!!~~")
+
+
+
+Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '다국어 기능을 위한 gettext, 하지만 평소에도 쓰면 좋아요',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp"))},
+             content: "
+PHP에서 gettext는 다국어를 위한 확장기능이며
+
+PHP에서 gettext기능을 이용하기 위해서는 활성화 되어 있어야 되며 phpinfo()에서 gettext부분이 있어야 됩니다.
+이부분에 없다면 php.ini 에서 extension=gettext.so(윈도우는 dll)부분에서 앞에 주석을 없애고 재시작 하세요
+phpinfo()에서 이렇게 한줄만 나오면 gettext를 사용할 수 있는 환경인것입니다.
+
+gettext기능은 원래 다국어 지원을 위해서 만들어진 기능으로 이렇게 모든 문자열을 한곳에서 처리하므로 각 언어에 맞게 수정한 파일들을 각 언어별 폴더에 넣어놓으면 모든 문자열이 각 언어로 바뀌게 됩니다.
+
+보통 프로그램을 보면 사용자에게 보이는 문자열(메뉴, 버튼의 텍스트, 애러-성공 메세지)이 곳곳에 쓰이는데
+gettext기능을 사용해서 다국어로 변환 할수 있고 통합적으로 관리 할 수 있습니다.
+
+예를 들면 gettext기능을 쓰지 않고 메세지 수정할 일이 생겼다고 한다면 이 텍스트가 들어있는게 100개라면 100개가 있는곳을 찾아서 전부 수정해주어야 겠지요?...  헉 소리 납니다.
+하지만 gettext기능을 이용한다면 po파일을 열어서 수정후 저장(mo파일) 하면 처리가 끝나게 됩니다.
+그러므로 다국어를 쓰지 않을때도 저런 수정관련을 위해서 써주면 매우 좋은 기능입니다.
+
+프로그램상에서는 gettext('문자열')로 쓰이게 되는데 gettext()대신에 짧게 _()로 사용하는게 일반적입니다.
+즉 _() = gettext() 입니다. 'gettext'라고 매번 쓰는것보다 '_'가 훨씬 간단하고 좋지요?
+_('문자열')로 사용하면 됩니다.
+
+워드프레스나 다른 프레임워크에서는 __()등의 함수('_'가 두개임)를 쓰는데 저 함수는 해당프레임워크에서 제공해주는 함수로서 PHP기본 gettext와는 다릅니다.
+물론 해당 프레임워크에서는 저것을 쓰는것이 더 편의성이 있고 맞는 방법이지만 다른것이라는 것만 인식하면 바뀌었을때 헷갈리는 일이 없겠지요?
+
+gettext의 세부 사용방법은 여기를 참고하세요
+
+http://php.net/manual/kr/book.gettext.php
+
+또한 gettext는 직접 작성하기보다는 po에디터라는 전용 에디터를 사용하는데
+www.poedit.net 로 받아서 사용하세요")
+
+
+Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: 'PHP Filter를 사용해서 깨끗한값만 받으세요',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp"))},
+             content: "
+
+프로그램에서 사용자의 잘못된 입력값을 중간에서 잡어서 잘못된 입력값이 입력되지 않게하고
+이를 사용자에게 잘 알려주는것은 프로그램의 기본입니다.
+
+
+그러나.........
+이런 기본도 못하는 프로그램이 많은것도 사실입니다.
+또한 이를 처리하기 위해서 각자의 방법으로 처리를 하게되는데 이는 제대로 검증되지 못하였기에 언제나
+불안요소를 안고 가게됩니다.
+
+그래서 나온것이 PHP에서는 Filter입니다.
+5.2버전 이전에는 PECL확장으로 존재했었고 5.2부터 기본에 포함되었으므로 기본에 포함된지 꽤 오래 되었는데
+아직도 제대로 쓰는곳을 보기가 힘드네요
+막강한 기능을 가진 Filter에 대해 일일히 설명하기는 힘들고
+여기 http://php.net/manual/kr/book.filter.php를 보시면 쉽게 이해될것입니다.
+
+이런 필터를 구현할 때 정규표현식으로 구현하는 경우가 많은데
+이를 이용하면 힘들게 정규표현식 쓸 필요도 없이 간단하게 모든 데이터를 필터링할 수 있습니다.
+Filter와 PDO bind를 쓰면 이상한 데이터가 들어가는걸 다 막을수 있습니다.
+
+깨끗한 데이터만 입력되는 제대로된 PHP프로그램을 제작하려면 Filter를 꼭 사용해주세요~~
+")
+
+
+
+
+Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: 'PHP에서 SQL-Server 데이터베이스 접속하기',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp"))},
+             content: "
+PHP는 어느 데이터베이스라도 접속확장이 있으며 이를 이용해서 해당 데이터베이스에 접속하여 프로그램을 제작하면 됩니다.
+PHP에서 SQL-Server 에 접속하여 사용하려면 역시 해당 드라이버가 필요한데 드라이버 이름은 sqlsrv 입니다.
+sqlsrv는 SQL-Server의 제작 회사인 microsoft에서 제공하므로 문서와 지원이 좋습니다.
+(5버전 때 제가 사용 할때만 해도 별로 자료가 없고 지원이 없었던걸 생각하면 지금은 너무나 좋습니다)
+
+<a href='https://github.com/microsoft/msphpsql' target='_blank'>sqlsrv github 문서</a>
+이 문서에 윈도우와 리눅스,MacOS에서 설치 방법이 다 있으며 윈도우가 아닌 경우가 많으니
+<a href='https://learn.microsoft.com/ko-kr/sql/connect/php/installation-tutorial-linux-mac?view=sql-server-ver16' target='_blank'>리눅스,MacOS에서 설치</a>
+에 있는대로만 따라하면 쉽게 설치 됩니다.
+sqlsrv를 설치하고 재시작이 잘 된 이후에 phpinfo()에서 sqlsrv가 있으면 성공입니다.
+
+윈도우의 주의할것은 경우 32비트, 64비트, ts인지 ,nts인지를 확인하여 맞는 dll을 쓰는것인데
+ts는 Thread Safety의 약자로 phpinfo에서 확인 할수 있습니다.
+
+이런 방법들을 통해 PHP로 SQL-Server에 접속하여 이용할수 있다.
+MySQL로만 사용했다면 PHP로 다른 데이터 베이스를 접근해서 사용하는 재미도 쏠쏠하다.
+
+특히 PDO로 접속하면 어떤 데이터 베이스던지 간에 똑같은 코딩스타일로 만들수 있는 매력이 있다.
+PDO로 어떤 데이터베이스던지 상관 없이 만드는 재미를 느껴보세요~~
+")
+
+
+
+
+Blog.create!(user_id:1, program_language_id: 3, blog_category_id:1, title: '젠투리눅스에서 커널컴파일 해보기',blog_picture_attributes: { picture: File.open(Rails.root.join("app", "assets", "images", "PHP-logo.webp"))},
+             content: "
+리눅스를 사용하는 또 하나의 재미인 커널 컴파일입니다.
+
+커널 컴파일이란?
+커널(Kernel)은 OS의 핵심으로 이를 바탕으로 여러 프로그램이 그 위에서 돌아가게 됩니다.
+컴파일(Compile) 은 소스코드를 컴퓨터가 알아듣게 변환하는 과정으로 이를 통해서 실제 돌아가게 되는 코드가 만들어 집니다.
+
+
+즉 커널컴파일이란 커널소스를 컴파일하여 커널을 생성하는 과정이라고 할 수 있습니다.
+OS의 핵심을 직접 컴파일 한다니?  윈도우에서는 느낄수 없는 재미지요
+그럼 어렵지 않냐고요?  물론 처음은 조금 어렵지만 몇번 하면 금세 익숙해질수 있습니다.
+쉽게 생각하면 커널소스를 컴파일을 하는것 뿐입니다.
+특히 젠투리눅스에서는 더 쉽습니다.
+
+그럼 커널 컴파일은 왜 하나요?
+
+리눅스 설치시 설치된 커널은 일반적인 모든 사양을 포함하게 되어있습니다.
+이말은 모든 하드웨어에서도 잘 돌아가게 된다는 말이지만 그를 위해서 내 컴에는 있지도 않은 하드웨어
+나에게는 필요없는 기능들도 다 포함되어 있어서 무겁다는 뜻이지요
+커널 컴파일을 통해 나에게 필요없는 것들은 빼고 딱 맞는 커널을 만들어서 최고의 성능을 낼 수있습니다
+
+그럼 한번 시작해볼까요?
+
+우선 커널 소스를 설치
+emerge gentoo-sources로 설치하면 쉽게 되지요
+설치후엔 cd /usr/src 로 이동하여 linux폴더가 있는지 확인합니다
+/usrc/src/linux 폴더가 최신소스이면 됩니다.
+
+
+다음은 emerge genkernel로 소스컴파일 쉽게하는 도구 설치
+genkernel 이 있으면 설정만 하면 컴파일은 그냥 자동으로 됩니다.
+genkernel --menuconfig all로 실행후 설정하고 나오면 컴파일이 진행됩니다.
+
+컴파일 완료후엔 /boot 디렉토리 가서 잘 완료되었는지 확인하고 Grub에 현재 버전으로 부팅되게 하면 됩니다.
+")
+
+=begin
+
+자기 컴퓨터(보통 local이라고 칭한다. 이하 로칼)를 이용해서 레일즈로 웹사이트를 만든후에 어떻게 해야할까?
+
+물론 우선 로칼에서 모든 테스트를 통과하여 애러 없이 제대로 운영이 가능하다고 판단된다면 이제 실서비스 진행할 차례이다.
+
+  이렇게 로칼에서 실서비스로 적용시키는것은 deploy(배치)라고 한다.
+
+  우선 실서비스를 진행할 서버컴퓨터가 있어야 하는데
+
+서버컴퓨터는 고가로 마련하는게 쉽지 않다.
+
+  그래서 보통 하나의 서버컴퓨터에서 여러 사용자가 이용하는 호스팅을 사용하는데
+
+레일즈 웹호스팅을 제공하는 업체는 찾기가 힘들다.
+
+
+
+  Cafe24에서 레일즈 호스팅을 제공하고 나도 예전에 썻었었는데 아직도 예전버전만 지원하여 이제는 거의 망한것 같은 느낌이다
+
+예전 저 버전으로 운영가능하다면 사용할수 있겠지만....... 힘들듯 하다.
+
+
+
+  또 하나로 heroku라는 것이 있는데
+
+이는 무료로 제공되는 호스팅으로 git로 올리면 적용되는 재미있고 편한 구조이다. 이런 서비스가 무료라니~~
+
+정말 사용하기 좋지만......... 서버가 미국에 있는것 같은데.. 그래서 여기(한국)서는 이용하기 힘들다.
+
+  물론 다 잘된다.  하지만 현 기술로는 태평양을 건너는데는 아직 시간이 필요하다.
+
+    응답속도가 느릴수 밖에 없다.
+
+      아메리카 대륙 거주자라면 heroku를 이용하여 무료 호스팅과 운영이 가능하다.
+
+
+
+        요즘 호스팅에서 뜨는것이 바로 가상서버이다.
+
+          호스팅과 비슷한 개념이지만 각 사용자마다 다른 서버를 이용하는것 처럼 모든 기능을 이용할수 있는 서비스이다.
+
+            이를 이용하면 레일즈 서비스를 제공하는데 별 문제가 없을것이다.
+
+              하지만 역시 단독 서버에 비하면 약간의 속도 저하가 있으므로 대량 서비스를 할때는 알맞지 않다.
+
+
+
+                가상서버도 그리 마음에 들지 않는다면
+
+단독서버를 마련하는것도 요즘은 그리 많은 비용이 들지 않는다.
+
+  월 10만원 이하로 월사용이 가능하다.
+
+  레일즈를 이용한 여러 서비스를 제공할것이라면 단독서버를 마련하는것이 비용은 높지만 효과는 최상일것이다.
+
+
+
+    레일즈 호스팅이 있으면 좋겠지만 현재 우리나라에서는 거의 없는 실정이고
+
+대신 가상서버나 단독서버를 이용하여 서비스를 제공할수 있다.
+
+  사실상 가상서버와 호스팅의 차이는 거의 없는 상태이므로 우선 가상서버를 이용하고
+
+여유가 된다면 단독서버로 레일즈 서비스를 제공할수 있을 것이다.
+=end
